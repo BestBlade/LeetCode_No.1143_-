eafdf8a5b0c789baf18b8456ee97bde15d744421
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-04-03 23:20:15 +0800

    Add files via upload

---
+++ leetcode_no1143_最长公共子序列_w1.cpp
@@ -0,0 +1,107 @@
+﻿/* ------------------------------------------------------------------|
+给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+
+一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+
+例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+
+ 
+
+示例 1：
+
+输入：text1 = "abcde", text2 = "ace"
+输出：3
+解释：最长公共子序列是 "ace" ，它的长度为 3 。
+示例 2：
+
+输入：text1 = "abc", text2 = "abc"
+输出：3
+解释：最长公共子序列是 "abc" ，它的长度为 3 。
+示例 3：
+
+输入：text1 = "abc", text2 = "def"
+输出：0
+解释：两个字符串没有公共子序列，返回 0 。
+ 
+
+提示：
+
+1 <= text1.length, text2.length <= 1000
+text1 和 text2 仅由小写英文字符组成。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-common-subsequence
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：12 ms, 在所有 C++ 提交中击败了92.94%的用户
+*	内存消耗：6.3 MB, 在所有 C++ 提交中击败了96.39%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+int longestCommonSubsequence(string text1, string text2) {
+    int m = text1.size();
+    int n = text2.size();
+    vector<int> dp(n + 1, 0);
+    vector<int> pre = dp;
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (text1[i] == text2[j]) {
+                dp[j + 1] = pre[j] + 1;
+            }
+            else {
+                dp[j + 1] = max(dp[j + 1], dp[j]);
+            }
+        }
+        pre = dp;
+    }
+    return dp[n];
+}
+
+int main() {
+    string t1 = "abcba";
+    string t2 = "abcbcba";
+    longestCommonSubsequence(t1, t2);
+}
